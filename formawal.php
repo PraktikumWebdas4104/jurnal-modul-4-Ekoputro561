b61b082307bec82f4d5d923629b2ca0285de1226
Eko Putro Abdrrahman Syakir <43515990+Ekoputro561@users.noreply.github.com>
2018-09-28 09:57:02 +0700

    Update formawal.php
    Eko Putro Abdurrahman Syakir
    6701174107

--- formawal.php
+++ formawal.php
@@ -1,5 +1,23 @@
 <?php
-
+  <form method="POST" action="proses.php"> 
+	<table>
+		<tr>
+			<td>Username</td>
+			<td></td>
+			<td><input type="text" name="username"></td>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td></td>
+			<td><input type="password" name="password"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td align="right"><input type="submit" name="submit" value="Log In"></td>
+		</tr>
+	</table>
+</form>
 
 
 ?>
